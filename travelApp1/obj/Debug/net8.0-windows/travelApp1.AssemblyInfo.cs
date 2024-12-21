24ddbc751b8aa0e3e9bf186d1ad4f90237e68180
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2024-12-21 19:38:12 +0700

    temp

--- travelApp1/obj/Debug/net8.0-windows/travelApp1.AssemblyInfo.cs
+++ travelApp1/obj/Debug/net8.0-windows/travelApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("travelApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8640c5c6ce291ae6a75f22e66c9e4fda134230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a0da065f1c9d97d50618b7a12052180f0e7e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("travelApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("travelApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
